cea2b9d32ae3e710cd331897021b720a7f43d389
Sachin Sharma <148564606+SachinDevX@users.noreply.github.com>
2024-11-04 11:20:46 +0530

    Update shop_page.dart

--- pages/shop_page.dart
+++ pages/shop_page.dart
@@ -1,3 +1,5 @@
+import 'package:coffee_app/components/coffee_tile.dart';
+import 'package:coffee_app/models/coffee.dart';
 import 'package:coffee_app/models/coffee_shop.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,35 +12,63 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  // Add coffee to cart
+  void addtocart(Coffee coffee) {
+    // Add coffee to cart
+    Provider.of<CoffeeShop>(context, listen: false).addItemtocart(coffee);
+
+    // Wait for 2 seconds, then show the alert dialog
+    Future.delayed(Duration(seconds: 2), () {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Successfully added to cart"),
+          content: Text("${coffee.name} has been added to your cart."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text("OK"),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final coffeeShop = Provider.of<CoffeeShop>(context);
 
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          children: [
-            Text(
-              "How would you like your coffee?",
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 25),
-            // List of coffee to buy
-            Expanded(
-              child: ListView.builder(
-                itemCount: coffeeShop.coffeeShop.length,
-                itemBuilder: (context, index) {
-                  final coffee = coffeeShop.coffeeShop[index];
-                  return ListTile(
-                    title: Text(coffee.name),
-                    subtitle: Text('\$${coffee.price}'),
-                    leading: Image.asset(coffee.imagepath),
-                  );
-                },
+    return Consumer<CoffeeShop>(
+      builder: (context, value, child) => SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              Text(
+                "How would you like your coffee?",
+                style: TextStyle(fontSize: 20),
               ),
-            ),
-          ],
+              const SizedBox(height: 25),
+              // List of coffee to buy
+              Expanded(
+                child: ListView.builder(
+                  itemCount: coffeeShop.coffeeShop.length,
+                  itemBuilder: (context, index) {
+                    // Get individual coffee
+                    Coffee eachCoffee = value.coffeeShop[index];
+                    return CoffeeTile(
+                      coffee: eachCoffee,
+                      icon: Icon(Icons.add),
+                      onPressed: () => addtocart(eachCoffee),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
